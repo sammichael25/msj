b8466237cab43427894731c991f79c1651937cc9
sammichael25 <sammichael25@gmail.com>
2017-04-10 11:35:19 -0400

    Edited
    added site

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/MSJ/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/MSJ/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/MSJ/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/MSJ/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/MSJ/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/MSJ/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/MSJ/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/MSJ/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/MSJ/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/MSJ/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/MSJ/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/MSJ/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/MSJ/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/MSJ/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/MSJ/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/MSJ/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'msj');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
